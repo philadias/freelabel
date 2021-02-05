c173235a97289cc07d0a6124b39236edf447daf7
Amy Tabb <amy.tabb@gmail.com>
2021-02-05 18:54:39 -0500

    fixed gitignore

--- python-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/atabb/git/freelabel2021/freelabel2-5/freelabel/python-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
